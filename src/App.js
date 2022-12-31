4db132e70b80076d9a58838dc377676aef2efc76
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2022-12-31 15:23:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import "./App.css";
+import Header from "./components/Header";
+import CreateArea from "./components/CreateArea";
+import { useState } from "react";
+import Note from "./components/Note";
+import Footer from "./components/Footer";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id){
+    setNotes(prevNotes =>{
+      return prevNotes.filter((noteItem, index)=>{
+        return index!==id
+      })
+    })
+  }
+
+  return (
+    <div className="App">
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return <Note 
+          key={index}
+          id={index}
+          title={noteItem.title}
+          content={noteItem.content}
+          onDelete={deleteNote}
+        />;
+      })}
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
